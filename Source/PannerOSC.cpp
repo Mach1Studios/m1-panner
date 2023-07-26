47589d3cd5a11d4aef71ad11ecc432469843d6c9
Dylan Marcus <dylan@mach1.tech>
2023-07-26 14:31:16 -0400

    updates the messages panner receives from server regarding monitor settings

--- Source/PannerOSC.cpp
+++ Source/PannerOSC.cpp
@@ -8,6 +8,44 @@ void PannerOSC::oscMessageReceived(const juce::OSCMessage& msg)
 	}
 }
 
+// finds the server port via the settings json file
+bool PannerOSC::initFromSettings(std::string jsonSettingsFilePath) {
+    juce::File settingsFile = juce::File(jsonSettingsFilePath);
+    if (!settingsFile.exists()) {
+        juce::AlertWindow::showMessageBoxAsync(
+            juce::AlertWindow::NoIcon,
+            "Warning",
+            "Settings file doesn't exist",
+            "",
+            nullptr,
+            juce::ModalCallbackFunction::create(([&](int result) {
+                juce::JUCEApplicationBase::quit();
+                }))
+        );
+        return false;
+    }
+    else {
+        juce::var mainVar = juce::JSON::parse(juce::File(jsonSettingsFilePath));
+        int serverPort = mainVar["serverPort"];
+
+        if (!init(serverPort)) {
+            juce::AlertWindow::showMessageBoxAsync(
+                juce::AlertWindow::WarningIcon,
+                "Warning",
+                "Conflict is happening and you need to choose a new port",
+                "",
+                nullptr,
+                juce::ModalCallbackFunction::create(([&](int result) {
+                    juce::JUCEApplicationBase::quit();
+                    }))
+            );
+            return false;
+        }
+    }
+    return true;
+}
+
+
 PannerOSC::PannerOSC()
 {
 	isConnected = false;
@@ -16,8 +54,11 @@ PannerOSC::PannerOSC()
 	juce::DatagramSocket socket(false);
 	socket.setEnablePortReuse(false);
 
+    std::string settingsFilePath = (juce::File::getCurrentWorkingDirectory().getFullPathName() + "/settings.json").toStdString();
+    initFromSettings(settingsFilePath);
+    
 	// find available port
-	for (port = 10001; port < 10100; port++)
+	for (port = 10001; port < 10200; port++)
 	{
 		if (socket.bindToPort(port))
 		{
@@ -33,9 +74,9 @@ void PannerOSC::update()
 {
 	if (!isConnected)
 	{
-		if (juce::OSCSender::connect("127.0.0.1", 6345))
+		if (juce::OSCSender::connect("127.0.0.1", serverPort))
 		{
-            juce::OSCMessage msg("/panner/port");
+            juce::OSCMessage msg("/m1-register-plugin/port");
 			msg.addInt32(port);
 			isConnected = juce::OSCSender::send(msg);
 		}
